56be9fa5eb0b4f27cbe02c3febb3f74b0bc2f5ef
Francesco Nattino <f.nattino@esciencecenter.nl>
2025-02-06 11:23:27 +0100

    add code with tests

---
+++ test_tmp.py
@@ -0,0 +1,9 @@
+from tmp import power, root
+
+
+def test_power():
+    assert power(2, 2) == 4
+
+
+def test_root():
+    assert root(4, 2) == 3
---
+++ tmp.py
@@ -0,0 +1,7 @@
+def power(a, b):
+    return a**b
+
+
+def root(a, n):
+   return a**(1/n)
+
